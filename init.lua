31b03161a7c2130628bc7c5b38159c18c9bab1c7
Drew_Pfeiler <drewcpfeiler@gmail.com>
2024-11-12 09:46:01 -0600

    Adds some more language servers

--- init.lua
+++ init.lua
@@ -657,7 +657,11 @@ require('lazy').setup({
       -- for you, so that they are available from within Neovim.
       local ensure_installed = vim.tbl_keys(servers or {})
       vim.list_extend(ensure_installed, {
-        'stylua', -- Used to format Lua code
+        'stylua',
+        'html',
+        'eslint',
+        'ts_ls',
+        'cssls', -- Used to format Lua code
       })
       require('mason-tool-installer').setup { ensure_installed = ensure_installed }
 
